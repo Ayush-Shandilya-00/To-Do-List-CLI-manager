36e1b6e5e5a1b53c91f8919d9196080306bf62de
Ayush-Shandilya-00 <shandilya.ayush.in@gmail.com>
2026-06-28 11:48:46 +0530

    Update main.py and README.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # To-Do List CLI manager
 A Python-based Command-Line Interface (CLI) application for managing daily tasks, featuring persistent data storage using file handling.
 
-# Working on this Project. i will give read me when the project finished intially
+# 👨‍💻 Working on this Project. i will give read me when the project finished intially
+
+# 👨‍💻 Working........
+
+
+
---
+++ main.py
@@ -0,0 +1,21 @@
+# ╔══════════════════════════════════════╗
+# ║     PROJECT: TO-DO LIST MANAGER      ║
+# ╚══════════════════════════════════════╝
+
+
+def menu():
+    print("─────────|Welcome to the Task Manager|─────────")
+    print("")
+    print("                  ❀ MENU ❀")
+    print("───────────────────────────────────────────────")
+    print("1> Add Task")
+    print("2> View All Tasks")  
+    print("3❯ Update Task")  
+    print("4❯ Delete Task") 
+    print("5❯ Exit")
+    return
+
+menu()
+
+
+
